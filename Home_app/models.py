c52a1bc01436f11a02f6080723935caabcb93c42
Tuo Liu <u6567008@anu.edu.au>
2020-04-15 21:42:01 +1000

    Edited database models

--- Home_app/models.py
+++ Home_app/models.py
@@ -7,7 +7,7 @@ class Tradie(models.Model):
     FirstName = models.CharField(max_length=20)
     LastName = models.CharField(max_length=20)
     Pass = models.CharField(max_length=20)
-    Email = models.CharField(max_length=30)
+    Email = models.EmailField
     Phone = models.CharField(max_length=10)
     Address1 = models.CharField(max_length=100)
     Address2 = models.CharField(max_length=100, null=True)
@@ -28,7 +28,7 @@ class Customer(models.Model):
     FirstName = models.CharField(max_length=20)
     LastName = models.CharField(max_length=20)
     Pass = models.CharField(max_length=20)
-    Email = models.CharField(max_length=30)
+    Email = models.EmailField
     Phone = models.CharField(max_length=10)
     Address1 = models.CharField(max_length=100)
     Address2 = models.CharField(max_length=100, null=True)
@@ -45,6 +45,7 @@ class Order(models.Model):
     OrderId = models.CharField(max_length=10, primary_key=True)
     OrderStatus = models.CharField(max_length=15)
     Category = models.CharField(max_length=20)
+    OrderDate = models.DateField
     TradieId = models.ForeignKey(Tradie, on_delete=models.CASCADE)
     CustomerId = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
