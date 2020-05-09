0a289920179a1870f9d1173e2d710b3092439bda
Li Yichen <u6499990@anu.edu.au>
2020-05-09 22:54:33 +1000

    linked head portrait between front and backend

--- Home_app/urls.py
+++ Home_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -21,3 +23,4 @@ urlpatterns = [
     path('updatehp/', views.updatehp),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
