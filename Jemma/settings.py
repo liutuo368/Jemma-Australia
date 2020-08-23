d0c3df4fa0c61018ca278d75cbf06ea932f2c37b
Yuqing Zhai <u6865190@anu.edu.au>
2020-08-23 21:53:20 +1000

    fix page can't load bug

--- Jemma/settings.py
+++ Jemma/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static').replace('\\','/'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
