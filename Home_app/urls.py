bbb36a8b6287c5647b42aaadcc5a7998c4a414ae
Yuqing Zhai <u6865190@anu.edu.au>
2020-09-09 15:39:46 +1000

    update urls

--- Home_app/urls.py
+++ Home_app/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('side_menu_customer', views.side_menu_customer),
     path('tradie_quotes', views.tradie_quotes),
     path('customer_profile', views.customer_profile),
-    path('customer_quote', views.customer_quote()),
-    path('customer_history', views.customer_history()),
-    path('customer_current_order', views.customer_current_order())
+    path('customer_quote', views.customer_quote),
+    path('customer_history', views.customer_history),
+    path('customer_current_order', views.customer_current_order)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
